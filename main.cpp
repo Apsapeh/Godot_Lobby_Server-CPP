909bfc24b9a6618787616a5b4512e3873ea9dea7
Apsapeh <89659777+Apsapeh@users.noreply.github.com>
2022-04-17 10:37:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,21 +9,21 @@ int main()
         echo_server.clear_access_channels(websocketpp::log::alevel::all);
         echo_server.clear_error_channels(websocketpp::log::alevel::all);
 
-        // Initialize Asio
+        // Инициализирует ASIO
         echo_server.init_asio();
 
-        // Register our message handler
-        echo_server.set_open_handler(bind(&_connected, ::_1));
-        echo_server.set_message_handler(bind(&_data_received, &echo_server, ::_1, ::_2));
-        echo_server.set_close_handler(bind(&_disconnected, ::_1));
+        // Обработчики событий, вызывают другие функции
+        echo_server.set_open_handler(bind(&_connected, ::_1));                              // Когда пользователь присоеденился (Не используется)
+        echo_server.set_message_handler(bind(&_data_received, &echo_server, ::_1, ::_2));   // Когда получено сообщение от пользователя
+        echo_server.set_close_handler(bind(&_disconnected, ::_1));                          // Когда пользователь отключился
 
-        // Listen on port 9002
+        // Слушет порт указаный в main.h
         echo_server.listen(PORT);
 
-        // Start the server accept loop
+        // Запускает серверный цикл приёма
         echo_server.start_accept();
 
-        // Start the ASIO io_service run loop
+        // Запускает io_service ASIO цикл
         echo_server.run();
     } catch (websocketpp::exception const & e) {
         std::cout << e.what() << std::endl;
@@ -32,6 +32,7 @@ int main()
     }
 }
 
+// Функция отправки, более сокращённая
 void send(websocketpp::connection_hdl hdl, opcode_value opcode, string message)
 {
     websocketpp::lib::error_code ec;
